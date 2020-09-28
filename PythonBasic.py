50a86a14ebb77a6503f22c130d047cda0bf34054
Archanak-12 <archana.k1294@outlook.com>
2020-09-28 21:11:29 +0530

    American development team

--- PythonBasic.py
+++ PythonBasic.py
@@ -1,61 +1,52 @@
-# a = 10
-# b = 10
-# if a > b:
-#     print("a is Bigger")
-# else:
-#     print("b Not Bigger")
-# if a <= b:
-#     print("a is Bigger")
-# else:
-# #     print("b Not Bigger")
-# a = 1
-# while a<=10:
-#     print(a)
-# a = 1
-# # print(1<=10)
-# while 10>10:
-# # while False:
-#     print(a)
-# evencount = 0
-# oddcount = 0
-# a = 1
-# while a<=100:
-#     if a%2 == 0:
-#         evencount = evencount+1
-#     else:
-#         oddcount = oddcount + 1
-#     a=a+1
-# print(evencount)
-# print(oddcount)
-# n = 4
-# a = 1
-# fact = 1
-# while a<=n:
-#         fact = fact*a
-#         a=a+1
-# print(fact)
-# n = 10
-# a = 1
-# natural = 0
-# while a<=n:
-#         natural = natural+a
-#         a=a+1
-#
-# print(natural)
 # a=1
-# # b=1
 # while a<=5:
 #     b = 1
 #     while b<=5:
-#         print(a,",",b)
+#         print(a,"*",b, "=", a*b)
 #         b = b+1
 #     a = a+1
-# print(a,",",b)
-a=1
-c = int
-while a<=5:
-    b = 1
-    while b<=5:
-        print(a,"*",b, "=", a*b)
-        b = b+1
-    a = a+1
+# for i in range(1, 6):
+#    for j in range(1, 6):
+#        if i == j and j == i:
+#            print("*",end="")
+#            i = i + 1
+#            j = j + 1
+#        else:
+#            print(" ",end="")
+#    print()
+# x = 0
+# y = 4
+# for i in range(5):
+#    for j in range(5):
+#        if i == x and j == y:
+#            print("*",end="")
+#            x = x + 1
+#            y = y - 1
+#        elif i == j:
+#            print("*",end="")
+#        else:
+#            print(end=" ")
+#    print()
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         #if (i+j)%2 == 0:
+#         #if i+j>=6:
+#         if i+j >= 6:
+#         #if i + j <= 6:
+#         #if i >= j:
+#         #if j <= i:
+#         #if j <= i:
+#         #if i <= j:
+#         #if i != j:
+#             print("* ",end="")
+#         else:
+#             print(" ",end="")
+#     print()
+for i in range(1, 6):
+    for j in range(1, 6):
+        #if i+j <= 6:
+        if j<=i:
+            print(j ,end="")
+        else:
+            print(" ",end="")
+    print()
